dc9f47dd89a9977f592a4cbc32876700e3db68ef
sonaligadekar20 <sonaliygadekar@gmail.com>
2023-10-19 15:22:02 +0530

    /health get request send

--- index.js
+++ index.js
@@ -6,6 +6,10 @@ app.use(express.json());
 
 const PORT = 5000;
 
+app.get('/health', (req, res)=>{
+    res.json({status: 'All good!'});
+})
+
 app.listen(PORT, () => {
     console.log('Server is running on port ${PORT}.')
 });
