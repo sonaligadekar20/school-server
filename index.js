0e1039b53e5c9ba67102678343f025faf0507268
sonaligadekar20 <sonaliygadekar@gmail.com>
2023-10-19 17:57:51 +0530

    /students get request added

--- index.js
+++ index.js
@@ -4,11 +4,20 @@ const app = express();
 
 app.use(express.json());
 
-const PORT = 5000;
+const PORT = 5000
 
+const students = [];
 app.get('/health', (req, res)=>{
-    res.json({status: 'All good!'});
-})
+    res.json({status: 'All good!, All set'});
+});
+
+app.get('/students', (req, res)=>{
+    res.json({
+        success: true,
+        data: students,
+        message: 'Successfully featched all students',
+    });
+});
 
 app.listen(PORT, () => {
     console.log('Server is running on port ${PORT}.')
