f555e198d0dbdeb832be06bddb04e4cb814ccb4d
sonaligadekar20 <sonaliygadekar@gmail.com>
2023-10-19 22:05:29 +0530

    added request

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const app = express();
 
 app.use(express.json());
 
-const PORT = 5000
+const PORT = 5000;
 
 const students = [];
 app.get('/health', (req, res)=>{
@@ -22,25 +22,78 @@ app.get('/students', (req, res)=>{
 app.post('/student',(req, res) => {
     const {name, age, mobile, email}= req.body;
 
-    const id = Math.floor(Math.random() * 100000) + 1;
+    if(!name){
+        return res.json({
+            success: false,
+            message: 'Name is required',
+        })
+    }
+
+    if(!age){
+        return res.json({
+            success: false,
+            message: 'Age is required',
+        })
+    }
+
+    if(!mobile){
+        return res.json({
+            success: false,
+            message: 'Mobile is required',
+        })
+    }
+
+    if(!email){
+        return res.json({
+            success: false,
+            message: 'Email is required',
+        })
+    }
+
+    const id = Math.floor(Math.random() * 1000) + 1;
 
     const newStudent = {
-        id: id,
-        name: name,
-        age: age,
-        mobile: mobile,
-        email: email,
+        id,
+        name,
+        age,
+        mobile,
+        email,
     }
 
     students.push(newStudent);
-
     res.json({
         success: true,
         data: newStudent,
-        message: 'Successfully added new student',
-        
+        message: 'Successfully added new student',  
     })
 })
+
+app.get('/student', (req, res) => {
+    const {id} = req.query;
+
+    let student = null;
+
+    students.forEach((stud)=>{
+        if(stud.id == id){
+            student = stud;
+        }
+    })
+
+    if(student == null){
+        return res.json({
+            success: false,
+            message: 'Student not found',
+        })
+    }
+
+    res.json({
+        success: true,
+        data: student,
+        message: 'Successfully fetched student',
+    })
+})
+
+
 
 app.listen(PORT, () => {
     console.log('Server is running on port ${PORT}.')
